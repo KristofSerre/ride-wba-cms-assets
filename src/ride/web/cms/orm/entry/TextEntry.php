e619e5f99a08571af1852d6e66d8c4a0edd323b8
Kristof Serré <kristofserre@kristofser.home>
2016-06-29 19:42:25 +0200

    bugfix in TextEntry where it could be null

---
+++ src/ride/web/cms/orm/entry/TextEntry.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ride\web\cms\orm\entry;
+
+use ride\application\orm\asset\entry\AssetEntry;
+use ride\application\orm\entry\TextEntry as OrmTextEntry;
+
+use ride\web\cms\text\Text;
+
+/**
+ * Data container for a text
+ */
+class TextEntry extends OrmTextEntry implements Text {
+
+	/**
+	 * Constructs a new instance
+	 * @param string $format Name of the format
+	 * @param string $body Body text
+	 * @return null
+	 */
+	public function __construct($format = null, $body = null) {
+	    $this->format = $format;
+	    $this->body = $body;
+
+	    $this->title = null;
+	    $this->image = null;
+	    $this->imageAlignment = null;
+	}
+
+	/**
+     * @param AssetEntry $image
+     * @return null
+     */
+    public function setImage(AssetEntry $image = null) {
+        if ($this->image === $image) {
+            return;
+        }
+
+        $this->image = $image;
+
+        if ($this->entryState === self::STATE_CLEAN) {
+            $this->entryState = self::STATE_DIRTY;
+        }
+    }
+
+}
